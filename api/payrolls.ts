f2a6beeea14cc62a3018c2fe823623455f2969ea
TBSK <tbsinarkarya@gmail.com>
2025-09-05 11:55:39 +0700

    Menambahkan folder api dan update koneksi database

--- api/payrolls.ts
+++ api/payrolls.ts
@@ -1,6 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import type { WeeklyPayroll } from "../src/types";
+import type { WeeklyPayroll } from "../types";
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
