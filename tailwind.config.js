0ea74b208a3f87e98de2f1745f5da6c4dcf1483c
TBSK <tbsinarkarya@gmail.com>
2025-09-08 11:47:05 +0700

    Menambahkan folder api dan update koneksi database

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-primary': '#4F46E5',   // indigo-600
+        'brand-secondary': '#10B981', // emerald-500
+      },
+    },
   },
   plugins: [],
 }
